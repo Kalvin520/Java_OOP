16a954b4a14053e58ab857c875df276db48da8dd
Kalvin <yaojaikai520@gmail.com>
2022-10-02 16:53:28 +0800

    update

---
+++ test/src/Inheritance/Guest.java
@@ -0,0 +1,26 @@
+package Inheritance;
+import java.util.Scanner;
+
+public class Guest {
+    public static void main(String[] args) {
+        ArrayList names = new ArrayList();
+        
+    }
+    static void collectNameTo(ArrayList names){
+        Scanner console = new Scanner(System.in);
+        while (true) {
+            System.out.print("訪客名稱：");
+            String name = console.nextLine();
+            if (name.equals("quit")) {
+                break;
+            }
+            names.add(name);
+        }
+    }
+    static void printUpperCase(ArrayList names){
+        for (int i = 0; i < names.size(); i++){
+            String name = (String) names.get(i);
+            System.out.println(name.toUpperCase());
+        }
+    }
+}
