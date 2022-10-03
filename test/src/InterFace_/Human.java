4e6073a54327cbf84400e26c559cf6aa4966fbc3
Kalvin <yaojaikai520@gmail.com>
2022-10-04 01:16:42 +0800

    add interface

--- test/src/InterFace_/Human.java
+++ test/src/InterFace_/Human.java
@@ -1,4 +1,15 @@
 package InterFace_;
 
-public class Human {
+public class Human implements Swimmer{
+    private String name;
+    public Human(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    @Override
+    public void swim(){
+        System.out.printf("人類 %s 游泳%n",name);
+    }
 }
