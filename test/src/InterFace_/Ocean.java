3ba3ddc69632d78309a3f4887f5b39c9a9edb542
Kalvin <yaojaikai520@gmail.com>
2022-10-04 02:17:36 +0800

    add interface_Ocean_main

--- test/src/InterFace_/Ocean.java
+++ test/src/InterFace_/Ocean.java
@@ -1,4 +1,13 @@
 package InterFace_;
 
 public class Ocean {
+    public static void main(String[] args) {
+        doswim(new Anemonefish("尼莫"));
+        doswim(new Shark("藍尼"));
+        doswim(new Human("賈斯丁"));
+        doswim(new Submarine("黃色一號"));
+    }
+    static void doswim(Swimmer swimmer){
+        swimmer.swim();
+    }
 }
