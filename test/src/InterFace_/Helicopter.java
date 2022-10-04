b97a5fd8d8f26549a73bdd454cf5e49e934b5f5f
Kalvin <yaojaikai520@gmail.com>
2022-10-04 10:46:38 +0800

    add interface_Diver

--- test/src/InterFace_/Helicopter.java
+++ test/src/InterFace_/Helicopter.java
@@ -1,4 +1,11 @@
 package InterFace_;
 
-public class Helicopter {
+public class Helicopter extends Airplane{
+    public Helicopter(String name){
+        super(name);
+    }
+    @Override
+    public void fly(){
+            System.out.printf("飛機 %s 在飛%n", name);
+    }
 }
--- test/src/InterFace_/Ocean.java
+++ test/src/InterFace_/Ocean.java
@@ -1,13 +1,13 @@
 package InterFace_;
 public class Ocean {
     public static void main(String[] args) {
-//        doswim(new Anemonefish("尼莫"));//介面的多型建立
-//        doswim(new Shark("藍尼"));
-//        doswim(new Human("賈斯丁"));
-//        doswim(new Submarine("黃色一號"));
-//        doswim2(new Shark("ss"));//繼承的多型建立並使用
-          doswim(new Seaplane("Air Force 1"));
-          doswim(new FlyingFish("flank"));
+        doswim(new Anemonefish("尼莫"));//介面的多型建立
+        doswim(new Shark("藍尼"));
+        doswim(new SwimPlayer("賈斯丁"));
+        doswim(new Submarine("黃色一號"));
+        //doswim2(new Shark("ss"));//繼承的多型建立並使用
+        doswim(new Seaplane("Air Force 1"));
+        doswim(new FlyingFish("flank"));
         }
     static void doswim(Swimmer swimmer){
         swimmer.swim();
