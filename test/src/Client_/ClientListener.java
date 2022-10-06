337bb5c48c6d2807e6e47884042545b4935c4a16
Kalvin <yaojaikai520@gmail.com>
2022-10-06 23:58:52 +0800

    renew clientListener

--- test/src/Client_/ClientListener.java
+++ test/src/Client_/ClientListener.java
@@ -1,6 +1,6 @@
 package Client_;
 
 public interface ClientListener {
-    void clientAdded(ClientEvent enent);
-    void clientRemoved(ClientEvent enent);
+    void clientAdded(ClientEvent event);//新增Client會呼叫這個方法
+    void clientRemoved(ClientEvent event);//減少Client會呼叫這個方法
 }
