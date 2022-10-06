eda8acc939881999dce99dc708e29bef0e2b050f
Kalvin <yaojaikai520@gmail.com>
2022-10-06 22:46:11 +0800

    add ClientEvevt

--- test/src/Client_/Client.java
+++ test/src/Client_/Client.java
@@ -1,4 +1,11 @@
 package Client_;
 
 public class Client {
+    public String ip;
+    public String name;
+    public void client(String ip,String name){
+        this.ip = ip;
+        this.name = name;
+    }
+
 }
