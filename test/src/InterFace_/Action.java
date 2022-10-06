44dcadc20349ad5e43c81155baa6499637b04e4f
Kalvin <yaojaikai520@gmail.com>
2022-10-06 20:41:52 +0800

    renew Class Game

--- test/src/InterFace_/Action.java
+++ test/src/InterFace_/Action.java
@@ -1,4 +1,9 @@
 package InterFace_;
 
 public interface Action {
+    public static final int STOP = 0;
+    public static final int Right = 1;
+    public static final int LEFT = 2;
+    public static final int UP = 3;
+    public static final int DOWN = 4;
 }
--- test/src/InterFace_/Game.java
+++ test/src/InterFace_/Game.java
@@ -1,4 +1,32 @@
 package InterFace_;
 
+
 public class Game {
+    public static void main(String[] args) {
+        play(Action.Right);
+        play(Action.UP);
+    }
+
+    public static void play(int action){
+        switch (action){
+            case Action.STOP:
+                System.out.println("播放停止動畫");
+                break;
+            case Action.Right:
+                System.out.println("播放向右動畫");
+                break;
+            case Action.LEFT:
+                System.out.println("播放向左動畫");
+                break;
+            case Action.UP:
+                System.out.println("播放向上動畫");
+                break;
+            case Action.DOWN:
+                System.out.println("播放向下動畫");
+                break;
+            default:
+                System.out.println("不支援此動作");
+        }
+    }
 }
+
